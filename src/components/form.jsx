7ab4d16a02766603927eea45f138a371961ff9b5
ERICKA <arielericka@gmail.com>
2024-08-09 17:27:05 -0600

    gg

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -27,7 +27,7 @@ function Formulario() {
     formData.append('description', description);
 
     try {
-      const response = await axios.post('https://corita1-production.up.railway.app/', formData, {
+      const response = await axios.post('https://corita1-production.up.railway.app/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
