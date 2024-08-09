f7f79085d8000b27e0639dbec97deeb8c4a71ad3
ERICKA <arielericka@gmail.com>
2024-08-09 14:59:04 -0600

    railway2

--- backend/server.cjs
+++ backend/server.cjs
@@ -28,7 +28,7 @@ app.use(cors({
 app.use(express.json()); // Para manejar datos JSON
 
 // Conectar a MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('Error al conectar a MongoDB:', err));
 
