b2bb9054a598e327727afc0d5eff2a6f5fa551a8
ERICKA <arielericka@gmail.com>
2024-08-09 15:16:20 -0600

    ayuda

--- backend/server.cjs
+++ backend/server.cjs
@@ -28,7 +28,7 @@ app.use(cors({
 app.use(express.json()); // Para manejar datos JSON
 
 // Conectar a MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Conectado a MongoDB'))
   .catch(err => console.error('Error al conectar a MongoDB:', err));
 
